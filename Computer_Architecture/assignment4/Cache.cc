0f5cc1ca66ee49f3cdffb9b584c3b87e12dcc114
Gonnnnn <geonkim@yonsei.ac.kr>
2022-06-16 18:24:16 +0900

    Assignment4 initial commit

---
+++ Computer_Architecture/assignment4/Cache.cc
@@ -0,0 +1,19 @@
+#include "Cache.h"
+
+// '_memory' defined in testCache.cc
+extern BYTE _memory[MEMSIZE];
+
+/*************************************************************************************/
+/* Assignment #4: Caches                                                             */
+/*                                                                                   */
+/* addr: target memory address                                                       */
+/* isWrite: true if write, false if read                                             */
+/* data: data to be written to addr if isWrite, data read from the cache if !isWrite */
+/*************************************************************************************/
+void Cache::access(const WORD addr, const bool isWrite, WORD *data) {
+  assert(addr % 4 == 0);
+  assert(data != nullptr);
+
+  // FIXME
+}
+
